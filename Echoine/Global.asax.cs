d8cfbd41a313207828666e6077993340cbefec6a
Rubens Mariuzzo <rubens@mariuzzo.com>
2013-06-05 09:41:17 -0400

    First working release of Echoine web app.

---
+++ Echoine/Global.asax.cs
@@ -0,0 +1,84 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Echoine
+{
+    public class EchoineApp : System.Web.HttpApplication
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(EchoineApp));
+        private static readonly ILog FormLogger = LogManager.GetLogger("Echoine.Form");
+
+        protected void Application_Start()
+        {
+            XmlConfigurator.Configure();
+            Logger.Debug("Configuring logging system...");
+            AreaRegistration.RegisterAllAreas();
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            Logger.Info("================================");
+            Logger.Info("       ~ Echoine is up ~        ");
+            Logger.Info("================================");
+        }
+
+        protected void Application_BeginRequest()
+        {
+            if (HttpContext.Current != null
+                && HttpContext.Current.Request != null
+                && HttpContext.Current.Request.Url != null)
+            {
+                // Log request.
+                var req = HttpContext.Current.Request;
+                Logger.DebugFormat("> {0}: {1}", req.HttpMethod, req.Url.PathAndQuery);
+                // Log request form data.
+                if (FormLogger.IsDebugEnabled) {
+                    var formData = new List<String>();
+                    if (req.Form!=null) {
+                        foreach (string f in req.Form)
+                            formData.Add(f + "=" + req.Form[f]);
+                    }
+                    FormLogger.DebugFormat("   form: {0}", String.Join(", ", formData));
+                }
+            }
+        }
+
+        protected void Application_EndRequest()
+        {
+            if (HttpContext.Current != null
+                && HttpContext.Current.Request != null
+                && HttpContext.Current.Response != null
+                && HttpContext.Current.Request.Url != null)
+            {
+                var req = HttpContext.Current.Request;
+                var res = HttpContext.Current.Response;
+                if (res.StatusCode == (int)HttpStatusCode.OK)
+                {
+                    Logger.DebugFormat("< {0}: {1} - {2}", req.HttpMethod, req.Url.PathAndQuery, res.StatusDescription);
+                }
+                else
+                {
+                    Logger.WarnFormat("< {0}: {1} - {2}", req.HttpMethod, req.Url.PathAndQuery, res.StatusDescription);
+                }
+
+                // Echo the headers.
+                res.ClearHeaders();
+                foreach (string kh in req.Headers)
+                    res.AddHeader(kh, req.Headers[kh]);
+
+                res.StatusCode = (int)HttpStatusCode.OK;
+                res.ClearContent();
+                res.Write(String.Format("{0}: {1}",req.HttpMethod, req.Url.PathAndQuery));
+            }
+        }
+    }
+
+
+}
---
+++ Echoine/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Echoine")]
+[assembly: AssemblyDescription("Web app that echo all HTTP response to response body and log file, useful for testing purposes.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mariuzzo")]
+[assembly: AssemblyProduct("Echoine")]
+[assembly: AssemblyCopyright("Copyright © Rubens Mariuzzo 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("538cbafd-2cfe-4960-9ae2-2fd6ba2ad313")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
